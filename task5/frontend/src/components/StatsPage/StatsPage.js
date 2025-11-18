92671af4072179eb0dd47d994a38d7236e0fa4ed
Daniil Makovetskii <d.makovetskii@g.nsu.ru>
2025-11-18 22:13:15 +0700

    Create StatsPage.js

---
+++ task5/frontend/src/components/StatsPage/StatsPage.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export default function StatsPage() {
+  const [stats, setStats] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/admin/kpis")
+      .then(res => res.json())
+      .then(setStats);
+  }, []);
+
+  return (
+    <div>
+      <h2>Статистика</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Страница</th>
+            <th>Посещения</th>
+            <th>Время</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats.map(s => (
+            <tr key={s.page_id}>
+              <td>{s.title}</td>
+              <td>{s.visits}</td>
+              <td>{s.time_spent}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
